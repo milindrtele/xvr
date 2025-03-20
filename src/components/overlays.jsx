fe5b475c26959145fb84e38e847994e39e13b5d6
metavian <team.metavian@gmail.com>
2025-03-20 12:35:59 +0530

    css changes and title

--- src/components/overlays.jsx
+++ src/components/overlays.jsx
@@ -3,6 +3,11 @@ import { useEffect, useState, useRef, useContext } from "react";
 export default function Overlays(props) {
   return (
     <div className="overlays">
+      <div className="title">
+        <p className="title_1">XVR 205</p>
+        <p className="title_2">Upcoming Headset</p>
+        <p className="title_3">India's First Consumer Grade VR Headset</p>
+      </div>
       <div className="metavian">
         {/* <div className="metavian_logo"></div> */}
         <div className="powered_by_metavian">
